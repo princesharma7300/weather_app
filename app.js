4f974f75735eb26dc080e183a13aba7a8aa030b9
Prince Sharma <55998593+princesharma7300@users.noreply.github.com>
2024-05-26 23:29:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    console.log('Hello'+' '+name)
+}
+greet('Prince');
+
+function cube(number){
+   return  number * number * number
+}
+cube(5);
+console.log(cube(5))
+
+let number1=cube(6)
+console.log(number1)
